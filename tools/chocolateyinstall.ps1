6f8ddc09048622904002bcbc0fd68edddd77499f
Eric Swann <eric.swann@q2ebanking.com>
2016-06-09 08:08:24 -0500

    adding initial content for ES choco package

---
+++ tools/chocolateyinstall.ps1
@@ -0,0 +1,31 @@
+﻿
+$ErrorActionPreference = 'Stop';
+
+$packageName= 'eventstore'
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'http://download.geteventstore.com/binaries/EventStore-OSS-Win-v3.6.2.zip'
+
+$packageArgs = @{
+  packageName   = $packageName
+  unzipLocation = $toolsDir
+  url           = $url
+}
+
+Install-ChocolateyZipPackage @packageArgs
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
